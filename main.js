ead66cf231a2c7b599c63edc829abbfebed8692b
Nestor Pineda <nestorpinedaelvira@gmail.com>
2021-11-04 11:19:27 +0100

    Variables declaradas

---
+++ main.js
@@ -0,0 +1,4 @@
+const input = document.querySelector("input");
+const addBtn = document.querySelector(".btn-add");
+const ul = document.querySelector("ul");
+const empty = document.querySelector(".empty");
